2b4aa590cba06878b02d0843f7a0f1b99a870c1b
Lei Jin <leijin.illinois@gmail.com>
2020-11-03 16:49:10 -0600

    Set up serializer

--- app/src/main/java/com/example/rentalchemy/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/rentalchemy/ui/main/MainViewModel.kt
@@ -1,7 +1,37 @@
 package com.example.rentalchemy.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rentalchemy.database.api.JsonServerApi
+import com.example.rentalchemy.database.api.PropertyRepository
+import com.example.rentalchemy.database.model.Property
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private val netProperties = MutableLiveData<List<Property>>()
+    private var userId = MutableLiveData<Int> ().apply { value = 1 }
+
+    private val jsApi = JsonServerApi.create()
+    private val propertyRepository = PropertyRepository(jsApi)
+
+
+
+    fun netProperties() = viewModelScope.launch(
+        context = viewModelScope.coroutineContext
+                + Dispatchers.IO
+    ) {
+        // Update LiveData from IO dispatcher, use postValue
+        netProperties.postValue(propertyRepository.getPropertyList(userId.value!!))
+    }
+
+
+    fun observeProperties(): LiveData<List<Property>> {
+        return netProperties
+    }
+
 }
