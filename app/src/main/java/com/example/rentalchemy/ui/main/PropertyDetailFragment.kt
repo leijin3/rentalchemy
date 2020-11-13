513b1c25669ac9fbac6f05f596a95a1fef4be1fc
Jolie Jenkins <jolielj@gmail.com>
2020-11-13 10:36:26 -0800

    add spinner widget to Property Detail

--- app/src/main/java/com/example/rentalchemy/ui/main/PropertyDetailFragment.kt
+++ app/src/main/java/com/example/rentalchemy/ui/main/PropertyDetailFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -13,10 +15,11 @@ class PropertyDetailFragment : Fragment(){
 
 
     private val viewModel: MainViewModel by activityViewModels()
+    private val propertyTypes: Array<String> by lazy {
+        resources.getStringArray(R.array.property_types)
+    }
 
     companion object {
-        private const val positionKey = "positionKey"
-
         fun newInstance() = PropertyDetailFragment()
     }
 
@@ -28,10 +31,16 @@ class PropertyDetailFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val propertyTypeSpinner : Spinner = view.findViewById(R.id.detail_property_type)
+        val propertyTypeAdapter = ArrayAdapter.createFromResource(this.requireContext(),
+                R.array.property_types, android.R.layout.simple_spinner_item)
+        propertyTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        propertyTypeSpinner.adapter = propertyTypeAdapter
+
         // Need a way to tell whether this was called from "Add Property" or by clicking on a
         // property from the list.  For Example, if MainViewModel.selectedProperty == null
         // but we'd need to set it to null on clicked "Add Property" button
-        if(true){
+        if(false){
             //TODO: Set name of button to "Save Property Detail"
             //On click, get stuff from form and call viewModel.addProperty
         } else {
@@ -43,7 +52,6 @@ class PropertyDetailFragment : Fragment(){
             var stateTV : TextView = view.findViewById(R.id.detail_state)
             var postalCodeTV : TextView = view.findViewById(R.id.detail_zip)
             var rentAmtTV : TextView = view.findViewById(R.id.detail_rent_amt)
-            // TODO: Do stuff for spinner for detail_property_type
             var sqftTV : TextView = view.findViewById(R.id.detail_sqft)
             var bedsTV : TextView = view.findViewById(R.id.detail_num_beds)
             var bathsTV : TextView = view.findViewById(R.id.detail_num_baths)
@@ -65,16 +73,20 @@ class PropertyDetailFragment : Fragment(){
                 costBasisTV.text = this.cost_basis
                 yearBuiltTV.text = this.year_built.takeLast(4)
                 dateAcqTV.text = this.date_acquired
+                setSpinnerToType(propertyTypeSpinner, this.propertyType)
             }
 
             //TODO: disable all the editTexts
 
             //detail_edit_saveBut.setOnClickListener{
-            // change name of button, enable editTexts, cancel this listener and make a new one
-            // that saves the edits (making sure to change the number strings back to ints)
-            // and re-sets the button name and click listener}
+            // change name of button, enable editTexts...}
 
 
         }
+    }
+
+    private fun setSpinnerToType(typeSpinner: Spinner, type : String){
+        val index = propertyTypes.indexOf(type)
+        typeSpinner.setSelection(index)
     }
 }
