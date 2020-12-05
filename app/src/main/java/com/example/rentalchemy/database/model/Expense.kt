ec10bf1bee08ce3f1e6fb58547fe9b234cac64d9
Lei Jin <leijin.illinois@gmail.com>
2020-12-05 10:42:26 -0600

    style: Auto-format

--- app/src/main/java/com/example/rentalchemy/database/model/Expense.kt
+++ app/src/main/java/com/example/rentalchemy/database/model/Expense.kt
@@ -5,6 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Expense(
-    var year: Int, var month: Int, var id: Long = Long.MIN_VALUE, var property_id: Long, var type: String, var date_spent: String,
-    var amount_spent: Float, var receipt_url: String
+    var year: Int,
+    var month: Int,
+    var id: Long = Long.MIN_VALUE,
+    var property_id: Long,
+    var type: String,
+    var date_spent: String,
+    var amount_spent: Float,
+    var receipt_url: String
 )
